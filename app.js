b414628446784ff4c8f2ab685f5c133b4939cd2a
zereight <duaxka7@naver.com>
2019-10-01 16:41:14 +0900

    2.9 MVC(1) router add

--- app.js
+++ app.js
@@ -4,26 +4,11 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import userRouter from "./router";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
 
 const app = express();
-
-const handleHome = (req, res) => {
-    console.log('This is homepage.');
-    res.send("This is Homepage.");
-};
-const handleProfile = (req, res) => {
-    console.log('This is profilepage.');
-    res.send("This is Profilepage.");
-};
-const betweenHome = (req, res, next) => { // middleware
-    console.log("Between");
-    next();
-};
-const betweenMiddleware = (req, res, next) => { // middleware
-    console.log("im mdware");
-    next();
-};
 
 app.use(helmet());
 app.use(morgan("dev"));
@@ -32,10 +17,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(betweenMiddleware);
-app.get('/', betweenHome, handleHome);
-app.get('/profile', handleProfile);
-
+app.use("/", globalRouter);
 app.use("/user", userRouter);
+app.use("/video", videoRouter);
 
 export default app;
---
+++ routers/globalRouter.js
@@ -0,0 +1,7 @@
+import express from "express";
+
+const globalRouter = express.Router();
+
+
+
+export default globalRouter;
