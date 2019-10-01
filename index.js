837188cedddcc6472da340608326f8a27a09d09b
zereight <duaxka7@naver.com>
2019-10-01 15:43:14 +0900

    Basic route

--- index.js
+++ index.js
@@ -4,5 +4,17 @@ const app = express();
 const handleListening = () => {
     console.log(`Listening port: 4000`);
 };
+const handleHome = (req, res) => {
+    console.log('This is homepage.');
+    res.send("This is Homepage.");
+};
+const handleProfile = (req, res) => {
+    console.log('This is profilepage.');
+    res.send("This is Profilepage.");
+};
+
+app.get('/', handleHome);
+app.get('/profile', handleProfile);
+
 
 app.listen(4000, handleListening);
