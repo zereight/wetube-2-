6516cc347be713827a3c34d1a980f04709c8e31b
zereight <duaxka7@naver.com>
2019-10-03 20:42:08 +0900

    #2.21 Home Controller Using Fake DB
    가짜 DB를생성하여서 main화면에 video목록을 출력한다.

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,5 +1,7 @@
+import {videos} from "../db";
+
 export const home = (req, res) => {
-    res.render("home", {pageTitle: "Home"});
+    res.render("home", {pageTitle: "Home", videos:videos});
 };
 export const upload = (req, res) => {
     res.render("upload", {pageTitle: "Upload"});
