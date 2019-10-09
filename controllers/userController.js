3a8ebed4783c1a450b5ff7931c2260d4ff257677
zereight <duaxka7@naver.com>
2019-10-09 13:05:17 +0900

    #6.2 Passport with authentication

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,22 @@
 import routes from "../routes";
+import User from "../models/User";
 
 export const getJoin = (req, res) => {res.render("join", {pageTitle: "Join"});}
-export const postJoin = (req, res) => {
+export const postJoin = async (req, res) => {
     const {body:{name, email, password, password2}} = req;
 
     if( password !== password2 ){
         res.status(400);
         res.render("join", {pageTitle: "Join"});
     }  else{
+        try {
+             // User.create안쓰는 이유는 create는 생성하고 db에 저장까지 함.
+             // 그러면 밑 register에서 이미 등록된 사용자라고 에러뜸.
+            const user = User({name, email});
+            await User.register(user, password); // 
+        } catch (error) {
+            console.log(error);
+        }
         res.redirect(routes.home);
     }
 
--- passport.js
+++ passport.js
@@ -2,3 +2,11 @@ import passport from "passport";
 import User from "./models/User";
 
 passport.use(User.createStrategy());
+
+// serialization은 어떤 정보를 쿠키에게 주느냐, 지금 클라이언트에 대해서 어떤 정보를 가질 수 있느냐를 뜻함
+// 쿠기에는 민감한정보없이 최대한 적게 정보를 담아야 안전함! id만넣자
+// deserialization은 어느 사용자인지 어떻게 찾는가?
+// passport야 userid만 담아서 쿠키만들어~
+passport.serializeUser( User.serializeUser() );
+// passport야 userid가 담긴 쿠기로 user찾아줘~
+passport.deserializeUser( User.deserializeUser() );
