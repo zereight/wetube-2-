08ad5f5b85ec9722e7777ce3b7d5bffc68b73c86
zereight <duaxka7@naver.com>
2019-10-09 12:33:07 +0900

    #6.1 Passport setting

--- init.js
+++ init.js
@@ -2,6 +2,7 @@ import app from "./app";
 import "./db"; // DB 로딩시키려고 그냥 import
 import "./models/Video"; // Model을 mongoose에 등록하는 역할.
 import "./models/Comment";
+import "./models/User";
 
 const handleListening = () => {
     console.log(`✅  Listening port: 4000`);
