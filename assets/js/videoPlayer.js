832946176ea3e44e2017db13e51d06418fd92899
zereight <duaxka7@naver.com>
2019-10-15 14:37:52 +0900

    #8.3 FullScreen, Exit FullScreen

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -3,6 +3,7 @@ const videoPlayer = document.querySelector("#jsVideoPlayer video");
 const playBtn = document.getElementById("jsPlayButton");
 
 const volumeBtn = document.getElementById("jsVolumeBtn");
+const fullScrnBtn = document.getElementById("jsFullScreen");
 
 function handlePlayClick() {
   if (videoPlayer.paused) {
@@ -24,9 +25,23 @@ function handleVolumeClick(){
     }
 }
 
+function exitFullScreen() {
+    fullScrnBtn.innerHTML = "<i class='fas fa-expand'></i>";
+    fullScrnBtn.addEventListener("click", goFullScreen);
+    document.webkitExitFullscreen();
+}
+
+function goFullScreen() {
+    videoContainer.webkitRequestFullscreen();
+    fullScrnBtn.innerHTML = "<i class='fas fa-compress'></i>";
+    fullScrnBtn.removeEventListener("click", goFullScreen);
+    fullScrnBtn.addEventListener("click", exitFullScreen);
+}
+
 function init() {
   playBtn.addEventListener("click", handlePlayClick);
   volumeBtn.addEventListener("click", handleVolumeClick);
+  fullScrnBtn.addEventListener("click", goFullScreen);
 }
 
 if (videoContainer) {
