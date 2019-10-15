25af7b6709a9fdb39618733b353b120cbed3d85a
zereight <duaxka7@naver.com>
2019-10-15 14:53:14 +0900

    #8.4 Total Time and Current Time

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -5,6 +5,9 @@ const playBtn = document.getElementById("jsPlayButton");
 const volumeBtn = document.getElementById("jsVolumeBtn");
 const fullScrnBtn = document.getElementById("jsFullScreen");
 
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
+
 function handlePlayClick() {
   if (videoPlayer.paused) {
     videoPlayer.play();
@@ -28,20 +31,67 @@ function handleVolumeClick(){
 function exitFullScreen() {
     fullScrnBtn.innerHTML = "<i class='fas fa-expand'></i>";
     fullScrnBtn.addEventListener("click", goFullScreen);
-    document.webkitExitFullscreen();
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+      } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+      } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+      } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+      }
 }
 
 function goFullScreen() {
-    videoContainer.webkitRequestFullscreen();
+    if (videoContainer.requestFullscreen) {
+        videoContainer.requestFullscreen();
+      } else if (videoContainer.mozRequestFullScreen) {
+        videoContainer.mozRequestFullScreen();
+      } else if (videoContainer.webkitRequestFullscreen) {
+        videoContainer.webkitRequestFullscreen();
+      } else if (videoContainer.msRequestFullscreen) {
+        videoContainer.msRequestFullscreen();
+      }
     fullScrnBtn.innerHTML = "<i class='fas fa-compress'></i>";
     fullScrnBtn.removeEventListener("click", goFullScreen);
     fullScrnBtn.addEventListener("click", exitFullScreen);
 }
 
+const formatDate = seconds => {
+    const secondsNumber = parseInt(seconds, 10);
+    let hours = Math.floor(secondsNumber / 3600);
+    let minutes = Math.floor((secondsNumber - hours * 3600) / 60);
+    let totalSeconds = secondsNumber - hours * 3600 - minutes * 60;
+  
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+    if (totalSeconds < 10) {
+      totalSeconds = `0${totalSeconds}`;
+    }
+    return `${hours}:${minutes}:${totalSeconds}`;
+  };
+  
+  function getCurrentTime() {
+    currentTime.innerHTML = formatDate(videoPlayer.currentTime);
+  }
+  
+  function setTotalTime() {
+    const totalTimeString = formatDate(videoPlayer.duration);
+    totalTime.innerHTML = totalTimeString;
+    setInterval(getCurrentTime, 1000);
+  }
+  
+  
+
 function init() {
   playBtn.addEventListener("click", handlePlayClick);
   volumeBtn.addEventListener("click", handleVolumeClick);
   fullScrnBtn.addEventListener("click", goFullScreen);
+  videoPlayer.addEventListener("loadedmetadata", setTotalTime); // video의 메타데이터가 로드될떄까지 기다리고 duration계산.
 }
 
 if (videoContainer) {
